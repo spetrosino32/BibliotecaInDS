cf4167e9cbbf4918f0a4d4c7be82f8158a22eb8b
spetrosino32 <s.petrosino32@studenti.unisa.it>
2025-11-24 09:44:59 +0100

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.bibliotecainds {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.bibliotecainds to javafx.fxml;
+    exports com.mycompany.bibliotecainds;
+}
+*/
