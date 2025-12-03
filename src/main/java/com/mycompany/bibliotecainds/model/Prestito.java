638c6e7fe228551371274672a31a398671ccd928
Utente <Utente@DESKTOP-74P3NRG>
2025-12-03 16:04:20 +0100

    Struttura base del progetto

--- src/main/java/com/mycompany/bibliotecainds/model/Prestito.java
+++ src/main/java/com/mycompany/bibliotecainds/model/Prestito.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package com.mycompany.bibliotecainds.model;
-
+import java.time.LocalDate;
 /**
  *
  * @author Giuseppe
  */
 public class Prestito {
-    
+    private Libro libro;
+    private LocalDate dataPrestito;
+
+    public Prestito(Libro libro, LocalDate dataPrestito) {
+        this.libro = libro;
+        this.dataPrestito = dataPrestito;
+    }
+
+    // getter e setter
 }
