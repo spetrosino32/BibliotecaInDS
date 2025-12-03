6b151fd96822a15fa4eded2e6dc9cd5433f1bcbb
Giuseppe <Giuseppe@DESKTOP-DH5Q43S.mshome.net>
2025-12-03 16:11:47 +0100

    Bacini

--- src/main/java/com/mycompany/bibliotecainds/model/Libro.java
+++ src/main/java/com/mycompany/bibliotecainds/model/Libro.java
@@ -5,14 +5,36 @@
  */
 package com.mycompany.bibliotecainds.model;
 
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
  * @author Giuseppe
  */
 public class Libro {
-    private String bigQuack;
+    private String titolo;
+    private List<String> autori;
+    private int anno;
+    private String isbn;
+    private int copie;
     
-    public Libro(String q){
-        bigQuack = q;
+    
+    public Libro(String titolo, List<String> autori, int anno, String isbn, int copie){
+        this.titolo = titolo;
+        this.autori=new ArrayList<>(autori);
+        this.anno=anno;
+        this.isbn=isbn;
+        this.copie=copie;
     }
+    
+    public String getTitolo() { return titolo; }
+    
+    public List<String> getAutori(){ return autori;}
+    
+    public int getAnno(){ return anno;}
+    
+    public String getIsbn(){return isbn;}
+    
+    public boolean isDisponibile(){ return copie > 0; }
+    
 }
