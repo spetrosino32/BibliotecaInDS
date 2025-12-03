549de8fde1d83e4fd752b0d7d3f4a2147041bf0f
Utente <Utente@DESKTOP-74P3NRG>
2025-12-03 16:04:07 +0100

    Struttura base del progetto

--- src/main/java/com/mycompany/bibliotecainds/model/Utente.java
+++ src/main/java/com/mycompany/bibliotecainds/model/Utente.java
@@ -4,11 +4,17 @@
  * and open the template in the editor.
  */
 package com.mycompany.bibliotecainds.model;
-
+//import java.time.LocalDate;
+//import java.util.List;
+import java.util.List;
 /**
  *
  * @author Giuseppe
  */
 public class Utente {
-    
+    private String nome;
+    private String cognome;
+    private int matricola;
+    private String email;
+    private List<Prestito> listaPrestiti;
 }
