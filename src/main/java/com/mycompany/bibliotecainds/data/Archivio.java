26552ea785205c7930f38156e79552389a9f6f38
Utente <Utente@DESKTOP-74P3NRG>
2025-12-03 17:09:55 +0100

    Struttura base del progetto

--- src/main/java/com/mycompany/bibliotecainds/data/Archivio.java
+++ src/main/java/com/mycompany/bibliotecainds/data/Archivio.java
@@ -5,11 +5,50 @@
  */
 package com.mycompany.bibliotecainds.data;
 
-/**
- *
- * @author Giuseppe
- */
-public class Archivio {
+import com.mycompany.bibliotecainds.model.Libro;
+import com.mycompany.bibliotecainds.model.Utente;
+import com.mycompany.bibliotecainds.model.Prestito;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Archivio implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private List<Libro> catalogoLibri;
+    private List<Utente> registroUtenti;
+
+    private List<Prestito> prestitiAttivi; 
+
+    private static Archivio instance;
+
+    private Archivio() {
+        catalogoLibri = new ArrayList<>();
+        registroUtenti = new ArrayList<>();
+        prestitiAttivi = new ArrayList<>();
+    }
+
+    public static Archivio getInstanza() {
+        if (instance == null) {
+            instance = new Archivio();
+        }
+        return instance;
+    }
+
+    public List<Libro> getCatalogoLibri() { return catalogoLibri; }
+    public List<Utente> getRegistroUtenti() { return registroUtenti; }
+    public List<Prestito> getPrestitiAttivi() { return prestitiAttivi; }
+
+    
+    public static void salva(String filePath) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(getInstanza());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void carica(String filePath) {
+    }
 }
 //ciao cenzo
